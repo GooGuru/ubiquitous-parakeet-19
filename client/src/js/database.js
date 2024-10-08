5b3832a16381bc5dfd43b85f42caf0237b603db8
GooGaru <xwilliam.morrisonx@gmail.com>
2024-10-08 13:20:23 -0400

    pushing code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // Open the database
+  const db = await openDB('jate', 1);
+  // Add or update the content in the database
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const result = await store.put({ content });
+  console.log('Data saved to the database:', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // Open the database
+  const db = await openDB('jate', 1);
+  // Get all entries from the object store
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const allContent = await store.getAll();
+  console.log('Data retrieved from the database:', allContent);
+  return allContent; // Return the retrieved content
+};
 
 initdb();
