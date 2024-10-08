b1f728d487be706919ca1b38fb9cb1725df20081
GooGaru <xwilliam.morrisonx@gmail.com>
2024-10-07 23:56:24 -0400

    still trying

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,24 +3,29 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: './client/src/js/index.js', // Ensure this path is correct
-      install: './client/src/js/install.js', // Ensure this path is correct
+      main: './client/src/js/index.js',
+   install: './client/src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'client/dist'), // Ensure this path is correct
+      path: path.resolve(__dirname, 'client/dist'),
     },
     plugins: [
+      // Generates an HTML file from a template
       new HtmlWebpackPlugin({
-        template: './client/src/index.html', // Ensure this path is correct
+        template: './src/index.html',
         filename: 'index.html',
       }),
+      // Generates a manifest file for the PWA
       new WebpackPwaManifest({
-        name: 'J.A.T.E',
+        name: 'Text Editor',
         short_name: 'TextEditor',
         description: 'A simple text editor that works offline',
         background_color: '#ffffff',
@@ -29,17 +34,19 @@ module.exports = () => {
         start_url: '.',
         icons: [
           {
-            src: path.resolve('client/src/images/icon.png'),
+            src: path.resolve('src/images/icon.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
         ],
       }),
+      // Injects the custom service worker
       new InjectManifest({
-        swSrc: './client/src/src-sw.js', // Ensure this path is correct
-        swDest: 'sw.js',
+        swSrc: './src-sw.js', // Your custom service worker file
+        swDest: 'sw.js', // The output service worker file
       }),
     ],
+
     module: {
       rules: [
         {
